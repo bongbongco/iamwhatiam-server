d81bbaf4561039195188639e9b90662403bf7b16
seungyonglee <bongbongco@gmail.com>
2018-11-11 06:12:52 +0900

    add connect database

---
+++ src/api/Hello/sayBye/sayBye.resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers = {
+    Query: {
+        sayBye: () => "Hey bye how are ya"
+    }
+};
+
+export default resolvers;
---
+++ src/ormConfig.ts
@@ -0,0 +1,17 @@
+import { ConnectionOptions } from "typeorm";
+
+const connectionOptions: ConnectionOptions = {
+    type: "postgres",
+    database: "nuber",
+    synchronize: true,
+    logging: true,
+    entities: [
+        "entities/**/*.*"
+    ],
+    host: process.env.DB_ENDPOINT,
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
+};
+
+export default connectionOptions;
