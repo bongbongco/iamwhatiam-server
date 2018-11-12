ab189d9dc15130786f12801fb30d6b4b72cf06f7
seungyonglee <bongbongco@gmail.com>
2018-11-13 05:51:11 +0900

    Add Place Entities

---
+++ src/entities/Verification.ts
@@ -0,0 +1,49 @@
+import { 
+    BaseEntity, 
+    Column, 
+    Entity, 
+    PrimaryGeneratedColumn, 
+    CreateDateColumn, 
+    UpdateDateColumn, 
+    BeforeInsert
+} from "typeorm";
+import { verificationTarget } from "src/types/types";
+
+const PHONE = "PHONE";
+const EMAIL = "EMAIL";
+
+@Entity()
+class Verification extends BaseEntity{
+    @PrimaryGeneratedColumn() id: number
+
+    @Column({ type: "text", enum: ["PHONE", "EMAIL"] })
+    target: verificationTarget;
+    
+    @Column({type: "text"})
+    payload: string;
+   
+    @Column({type: "text"})
+    key: string;
+
+    @Column({
+        type: "boolean", 
+        default: false
+    })
+    used: boolean;
+
+    @CreateDateColumn() createdAt: string;
+    @UpdateDateColumn() updatedAt: string;
+
+    @BeforeInsert()
+    createKey(): void {
+        if(this.target === PHONE) {
+            Math.floor(Math.random() * 100000).toString()
+        } else if(this.target === EMAIL) {
+            this.key = Math.random()
+                .toString(36)
+                .substr(2);
+        }
+    }
+}
+
+export default Verification;
