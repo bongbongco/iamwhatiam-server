89d83f757ed74e2a37550a86c07a50fd864601ff
seungyonglee <bongbongco@gmail.com>
2018-11-18 19:02:55 +0900

    Add Send SMS

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -6,10 +6,8 @@ import {
     CreateDateColumn, 
     UpdateDateColumn, 
     BeforeInsert,
-    ManyToOne
 } from "typeorm";
-import { verificationTarget } from "src/types/types";
-import User from "./User";
+import { verificationTarget } from "../types/types";
 
 const PHONE = "PHONE";
 const EMAIL = "EMAIL";
@@ -27,22 +25,13 @@ class Verification extends BaseEntity{
     @Column({type: "text"})
     key: string;
 
-    @Column({
-        type: "boolean", 
-        default: false
-    })
-    used: boolean;
-
-    @ManyToOne(type => User, user => user.verifications)
-    user: User;
-
     @CreateDateColumn() createdAt: string;
     @UpdateDateColumn() updatedAt: string;
 
     @BeforeInsert()
     createKey(): void {
         if(this.target === PHONE) {
-            Math.floor(Math.random() * 100000).toString()
+            this.key = Math.floor(Math.random() * 100000).toString()
         } else if(this.target === EMAIL) {
             this.key = Math.random()
                 .toString(36)
