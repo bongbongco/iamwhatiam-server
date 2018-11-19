b5f549bd236f48ff016bde8a01c3d41c62bcc370
seungyonglee <bongbongco@gmail.com>
2018-11-19 23:03:56 +0900

    Add JWT

--- src/api/User/CompletePhoneVerification/CompletePhoneVerification.resolvers.ts
+++ src/api/User/CompletePhoneVerification/CompletePhoneVerification.resolvers.ts
@@ -5,6 +5,7 @@ import {
 } from "../../../types/graph";
 import Verification from "../../../entities/Verification";
 import User from "../../../entities/User";
+import createJWT from "../../../utils/createJWT";
 
 const resolvers: Resolvers = {
     Mutation: {
@@ -42,10 +43,11 @@ const resolvers: Resolvers = {
                 if(user) {
                     user.verifiedPhoneNumber = true;
                     user.save();
+                    const token = createJWT(user.id);
                     return {
                         ok: true,
                         error: null,
-                        token: "Coming soon"
+                        token
                     };
                 } else {
                     return {
--- src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -1,6 +1,7 @@
 import { Resolvers } from "../../../types/resolvers";
 import { EmailSignUpMutationArgs, EmailSignUpRespose } from "../../../types/graph";
 import User from "../../../entities/User";
+import createJWT from "../../../utils/createJWT";
 
 const resolvers: Resolvers = {
     Mutation: {
@@ -12,17 +13,19 @@ const resolvers: Resolvers = {
             try {
                 const existingUser = await User.findOne({ email });
                 if(existingUser) {
+                    const token = createJWT(existingUser.id)
                     return {
                         ok: false,
                         error: "You should log in instead",
-                        token: null
+                        token
                     }
                 } else {
                     const newUser = await User.create({...args}).save();
+                    const token = createJWT(newUser.id); 
                     return {
                         ok: true,
                         error: null,
-                        token: "Coming soon"
+                        token
                     }
                 }
             } catch(error) {
