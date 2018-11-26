a7730211f7db5b73f0ea019a356bdbe90ff6bf8b
seungyonglee <bongbongco@gmail.com>
2018-11-26 21:36:16 +0900

    Edit UserMyProfile - password field

--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -1,4 +1,5 @@
 import { Resolvers } from "../../../types/resolvers";
+import cleanNullArgs from "../../../utils/cleanNullArg";
 import privateResolver from "../../../utils/privateResolver";
 import { UpdateMyProfileMutationArgs, UpdateMyProfileResponse } from "../../../types/graph";
 import User from "../../../entities/User";
@@ -11,13 +12,18 @@ const resolvers: Resolvers = {
             { req }
             ): Promise<UpdateMyProfileResponse> => {
                 const user: User = req.user;
-                const notNull = {};
+                const notNull:any = cleanNullArgs(args);
                 Object.keys(args).forEach(key => {
                     if(args[key] !== null){
                         notNull[key] = args[key];
                     }
                 });
                 try {
+                    if(notNull.password !== null) {
+                        user.password = notNull.password;
+                        user.save();
+                        delete notNull.password;
+                    }
                     await User.update({id: user.id}, { ...notNull });
                     return {
                         ok: true,
