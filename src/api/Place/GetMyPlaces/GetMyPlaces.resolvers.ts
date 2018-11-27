f97c946c6e75ae847aa2cf9a92334f148f7ce375
seungyonglee <bongbongco@gmail.com>
2018-11-27 20:47:30 +0900

    Add GetMyPlaces

---
+++ src/api/Place/GetMyPlaces/GetMyPlaces.resolvers.ts
@@ -0,0 +1,42 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import { GetMyPlacesResponse } from "../../../types/graph";
+import User from "../../../entities/User";
+
+const resolvers: Resolvers = {
+    Query: {
+        GetMyPlaces: privateResolver(async(
+            _,
+            __,
+            { req }
+        ): Promise<GetMyPlacesResponse> => {
+            const user = await User.findOne(
+                {id: req.user.id}, 
+                {relations: ["places"]}
+            )
+            try {
+                if(user) {
+                    return  {
+                       ok: true,
+                       places: user.places,
+                       error: null 
+                    };
+                } else {
+                    return {
+                        ok: false,
+                        places: null,
+                        error: "User not found"
+                    };
+                }
+            } catch(error) {
+                return {
+                    ok: false,
+                    places: null,
+                    error: null
+                };
+            }
+        })
+    }
+};
+
+export default resolvers;
